300c4d780365e5d7e8239672bc607dd39a55f4d9
enneract9 <ottoblau@yandex-team.ru>
2024-06-05 00:01:16 +0500

    first commit

---
+++ Financial planner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Financial planner
+//
+//  Created by @_@ on 04.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
